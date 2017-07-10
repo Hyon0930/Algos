e09071417129321b8a9d59037621c2ffe6b3a5f0
HK88 <hyun.kim.930@gmail.com>
2017-07-11 00:10:23 +0800

    Add files via upload
    Find a sub sequence in the input list of which sum is zero.

---
+++ subSumZero.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+
+def sum2zero(S):
+	
+	X = np.array([])
+	for i in range(0,len(S)):
+		j = i
+		check = False
+		while check == False:
+			j = j +1
+			if j == len(S):
+				break
+			if sum(S[i:j]) == 0:
+				X = np.append(X,(i,j))
+				Check = True
+	print X
+
+
+	return X
+
+
+S = [-1, 3, -4, 1, 2, 1, 4, 6]
+
+sum2zero(S)
